6e248d3401d7632c7129bd13ad38c983085b979e
David Watson <81671570+Nabijonov-Otabek-19@users.noreply.github.com>
2021-03-31 16:46:28 +0500

    JUST DO IT
    TO'XTASH YO'Q FAQAT OLDINGA

---
+++ Python and Opencv projects/Simple motion detection.py
@@ -0,0 +1,29 @@
+import cv2
+
+# ODDIY SODDA MOTION DETECTION
+
+# BACKGROUNDNI OLIB TASHLASH
+
+# fgbgl = cv2.createBackgroundSubtractorKNN()  # Shovqinsiz salgina
+
+# Sal shovqinliroq
+fgbgl = cv2.createBackgroundSubtractorMOG2(detectShadows=True)
+
+cam = cv2.VideoCapture("count.mp4")
+
+while 1:
+    ret, frame = cam.read()
+
+    # Backgroundni olib tashlash
+    fgmask = fgbgl.apply(frame)
+
+    _, thresh = cv2.threshold(fgmask, 254, 255, cv2.THRESH_BINARY)
+
+    cv2.imshow('frame', frame)
+    cv2.imshow('fgmask', thresh)
+
+    if cv2.waitKey(1) == 27:
+        break
+
+cam.release()
+cv2.destroyAllWindows()
